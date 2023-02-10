c2611025acb50796ee73293dc1ff2022d59bc3c0
bahuyit2905 <bahuynguyen1997@gmail.com>
2023-02-10 08:24:55 +0700

    cars

---
+++ baitap/car.js
@@ -0,0 +1,15 @@
+function controlCarRight() {
+    document.getElementById("cars").src = "images/right.jpg";
+}
+
+function controlCarLeft() {
+    document.getElementById("cars").src = "images/left.jpg"
+}
+
+function controlCarTop() { 
+    document.getElementById("cars").src = "images/top.jpg"
+}
+
+function controlCarBottom(){
+    document.getElementById("cars").src = "images/bottom.jpg"
+}
